6af22d42a8909fcb435532240be844af97fc8c02
Ken Tang <kenftang@gmail.com>
2023-01-09 22:42:24 +0000

    Add player jump and movement Add prototype level
    Added rudimentary player jump and movement
    to PlayerMove class.
    
    Added a prototype level using the unity tile map.

--- Assets/Scripts/playerMove.cs
+++ Assets/Scripts/playerMove.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class playerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rig;
+    float playerSpeedBase = 2f, playerJumpForceBase = 10f;
+    Vector2 mask;
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey("space"))
+        {
+            Jump();
+        }
+        if(Input.GetAxis("Horizontal") !=0 )
+        {
+            Move(Input.GetAxis("Horizontal"));
+        }
+    }
+
+    void Jump()
+    {
+        //TODO Method to check player is on ground
+        rig.velocity = Vector2.up * playerJumpForceBase;
+    }
+
+    void Move(float playerDir)
+    {
+        mask[0] = playerDir * playerSpeedBase;
+        mask[1] = rig.velocity.y;
+        rig.velocity = mask;
     }
 }
