93f5f033dc2f83122cb67472e09d0458a5f0e175
Ken Tang <kenftang@gmail.com>
2023-01-12 22:13:29 +0000

    Add RegrowCrop coroutine
    On fulling harvesting fruit, HarvestFruit now
    starts coroutine RegrowCrop. This waits for a
    period of time, and then sets harvestProgress
    to the starting value, sets isHarvestable to
    true and reenables fruitSprite.

--- Assets/Scripts/cropProperties.cs
+++ Assets/Scripts/cropProperties.cs
@@ -8,6 +8,8 @@ public class CropProperties : MonoBehaviour
     Collider2D stalkCol;
     SpriteRenderer fruitSprite;
     int harvestProgress = 80;
+    float timeToGrow = 2.5f;
+    WaitForSecondsRealtime delay = new WaitForSecondsRealtime(2.5f);
     
     void Start()
     {
@@ -19,6 +21,8 @@ public class CropProperties : MonoBehaviour
             if(sprite.name == "FruitPosition")
                 fruitSprite = sprite;
         }
+
+        // delay = new WaitForSeconds(timeToGrow);
     }
 
     public int HarvestFruit(int harvestPower)
@@ -30,6 +34,7 @@ public class CropProperties : MonoBehaviour
             Debug.Log("Fruit Harvested!");
             isHarvestable = false;
             fruitSprite.enabled = false;
+            StartCoroutine(RegrowCrop(timeToGrow)); 
         }
         return harvestProgress;
     }
@@ -37,5 +42,18 @@ public class CropProperties : MonoBehaviour
     public bool CheckHarvestable()
     {
         return isHarvestable;
+    }
+
+    IEnumerator RegrowCrop(float cropGrowingTime)
+    {
+        Debug.Log("RegrowCrop Coroutine started. Waiting.");
+        // yield return new WaitForSecondsRealtime(cropGrowingTime);
+        yield return delay;
+        Debug.Log("Plant is growing");
+        harvestProgress = 80;
+        isHarvestable = true;
+        fruitSprite.enabled = true;
+        Debug.Log("Plant has finished growing!");
+        yield return null;
     }
 }
